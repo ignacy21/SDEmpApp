b4a3edc1c88968e0a6cdf54b6042652c37115144
ignacy (air) <ignacyszpilowski@gmai.com>
2023-12-12 13:59:34 +0100

    Changing way that Localization will be stored in app (only in flyway)

--- src/main/resources/db/migrations/V1_3__SDEmpApp_job_advertisement.sql
+++ src/main/resources/db/migrations/V1_3__SDEmpApp_job_advertisement.sql
@@ -1,15 +1,19 @@
 CREATE TABLE job_advertisement
 (
-    job_advertisement_id    SERIAL                                                                              NOT NULL,
-    localization            VARCHAR(32)                                                                         NOT NULL,
-    languages               TEXT                                                                                NOT NULL,
-    duties                  TEXT                                                                                NOT NULL,
-    form_of_work            VARCHAR(32) CHECK (form_of_work IN ('HYBRID', 'IN_PLACE', 'REMOTE', 'FIT')),
-    company_id              INT                                                                                 NOT NULL,
+    job_advertisement_id SERIAL NOT NULL,
+    localization         INT    NOT NULL,
+    languages            TEXT   NOT NULL,
+    duties               TEXT   NOT NULL,
+    form_of_work         VARCHAR(32) CHECK (form_of_work IN ('HYBRID', 'IN_PLACE', 'REMOTE', 'FIT')),
+    company_id           INT    NOT NULL,
+    skill_level             VARCHAR(32) CHECK (form_of_work IN ('INTERNSHIP', 'JUNIOR', 'MID', 'SENIOR')),
     PRIMARY KEY (job_advertisement_id),
     CONSTRAINT fk_job_advertisement_company
         FOREIGN KEY (company_id)
-            REFERENCES company (company_id)
+            REFERENCES company (company_id),
+    CONSTRAINT fk_job_advertisement_localization
+        FOREIGN KEY (localization_id)
+            REFERENCES localization (localization_id)
 
 
 );
