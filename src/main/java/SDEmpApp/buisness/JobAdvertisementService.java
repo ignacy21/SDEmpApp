a68d5f32c58cfd3110375ddae5c39310089487de
ignacyszpilowski <ignacyszpilowski@gmail.com>
2024-03-27 09:13:36 +0100

    removing redundant methods, extracting predicates to separate methods

--- src/main/java/SDEmpApp/buisness/JobAdvertisementService.java
+++ src/main/java/SDEmpApp/buisness/JobAdvertisementService.java
@@ -1,31 +1,29 @@
 package SDEmpApp.buisness;
 
-import SDEmpApp.api.dto.LocalizationDTO;
-import SDEmpApp.api.dto.auxiliary.*;
-import SDEmpApp.api.dto.auxiliary.enums.Experience;
+import SDEmpApp.api.dto.auxiliary.LanguageDTO;
+import SDEmpApp.api.dto.auxiliary.SeniorityDTO;
+import SDEmpApp.api.dto.auxiliary.SkillDTO;
 import SDEmpApp.api.dto.auxiliary.enums.Language;
 import SDEmpApp.api.dto.auxiliary.enums.Skill;
 import SDEmpApp.api.dto.finalQueriesDTO.JobAdvertisementFinalFindQueryDTO;
 import SDEmpApp.buisness.DAO.JobAdvertisementDAO;
 import SDEmpApp.domain.JobAdvertisement;
 import SDEmpApp.domain.Localization;
-import SDEmpApp.infrastructure.database.repository.mapper.LocalizationEntityMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 import java.util.function.Predicate;
 
 @Service
 @AllArgsConstructor
 public class JobAdvertisementService {
-    private final JobAdvertisementDAO jobAdvertisementDAO;
-    private final LocalizationService localizationService;
 
-    private final LocalizationEntityMapper localizationEntityMapper;
+    private final JobAdvertisementDAO jobAdvertisementDAO;
+
+    private final LocalizationService localizationService;
 
     public JobAdvertisement createJobAdvertisement(JobAdvertisement jobAdvertisement) {
         return jobAdvertisementDAO.createJobAdvertisement(jobAdvertisement);
@@ -37,93 +35,6 @@ public class JobAdvertisementService {
 
     public JobAdvertisement updateJobAdvertisement(JobAdvertisement jobAdvertisement) {
         return jobAdvertisementDAO.updateJobAdvertisement(jobAdvertisement);
-    }
-
-    public List<JobAdvertisement> findByFormOfWork(FormOfWorkDTO formOfWorkDTO) {
-        String formOfWork = formOfWorkDTO.getFormOfWork().name();
-        return jobAdvertisementDAO.findByFormOfWork(formOfWork);
-    }
-
-    public List<JobAdvertisement> findBySkills(SkillDTOs skillDTOs) {
-        return skillDTOs.getSkills().stream()
-                .map(SkillDTO::getSkill)
-                .map(Skill::name)
-                .map(jobAdvertisementDAO::findBySkill)
-                .flatMap(List::stream)
-                .distinct()
-                .toList();
-    }
-
-    public List<JobAdvertisement> findOnlyBySpecifiedSkills(SkillDTOs skillDTOs) {
-        List<String> listSkillsAsStrings = skillDTOs.getSkills().stream()
-                .map(SkillDTO::getSkill)
-                .map(Skill::name)
-                .toList();
-
-        return listSkillsAsStrings.stream()
-                .map(jobAdvertisementDAO::findBySkill)
-                .flatMap(List::stream)
-                .distinct()
-                .filter(jobAd -> new HashSet<>(listSkillsAsStrings).containsAll(
-                        Arrays.stream(jobAd.getSkillsNeeded()
-                                .split(";")).toList()))
-                .toList();
-    }
-
-    public List<JobAdvertisement> findByLanguages(LanguageDTOs languageDTOs) {
-        return languageDTOs.getLanguageDTOs().stream()
-                .map(LanguageDTO::getLanguage)
-                .map(Language::name)
-                .map(jobAdvertisementDAO::findByLanguage)
-                .flatMap(List::stream)
-                .distinct()
-                .toList();
-    }
-
-    public List<JobAdvertisement> findBySpecifiedLanguages(LanguageDTOs languageDTOs) {
-        List<String> languagesAsStrings = languageDTOs.getLanguageDTOs().stream()
-                .map(LanguageDTO::getLanguage)
-                .map(Language::name)
-                .toList();
-
-        return languagesAsStrings.stream()
-                .map(jobAdvertisementDAO::findByLanguage)
-                .flatMap(List::stream)
-                .distinct()
-                .filter(jobAdvert -> new HashSet<>(languagesAsStrings).containsAll(
-                        Arrays.stream(jobAdvert.getLanguages().split(";")).toList()
-                ))
-                .toList();
-    }
-
-    public List<JobAdvertisement> findJobAdvertisementByLocalization(LocalizationDTO localizationDTO) {
-        Localization findLocalization = localizationService.findLocalization(localizationDTO);
-        return jobAdvertisementDAO.findByLocalization(findLocalization);
-    }
-
-    public List<JobAdvertisement> findJobAdvertisementByExperience(ExperienceDTO experienceDTO) {
-        int ordinal = experienceDTO.getExperience().ordinal();
-        return Arrays.stream(Experience.values())
-                .filter(x -> x.ordinal() <= ordinal)
-                .map(Experience::getExperience)
-                .map(jobAdvertisementDAO::findByExperience)
-                .flatMap(List::stream)
-                .distinct()
-                .toList();
-    }
-
-    public List<JobAdvertisement> findJobAdvertisementBySalary(SalaryDTO salary) {
-        return jobAdvertisementDAO.isSalaryBetweenRequiredSalary(salary.getSalary());
-    }
-
-    public List<JobAdvertisement> findJobAdvertisementBySeniority(SeniorityDTOs seniorityDTOs) {
-        return seniorityDTOs.getSeniorityDTOs().stream()
-                .map(SeniorityDTO::getSeniority)
-                .map(Enum::name)
-                .map(jobAdvertisementDAO::findBySeniority)
-                .flatMap(List::stream)
-                .distinct()
-                .toList();
     }
 
     public List<JobAdvertisement> criteriaApiFindQuery(
@@ -146,18 +57,24 @@ public class JobAdvertisementService {
             JobAdvertisementFinalFindQueryDTO finalQuery
     ) {
         Localization localization = finalQuery.getLocalizationDTO() != null ?
-                localizationService.findLocalization(finalQuery.getLocalizationDTO()) : null;
+                localizationService.findLocalization(finalQuery.getLocalizationDTO()) :
+                null;
         String formOfWork = finalQuery.getFormOfWorkDTO() != null ?
-                finalQuery.getFormOfWorkDTO().getFormOfWork().name() : null;
+                finalQuery.getFormOfWorkDTO().getFormOfWork().name() :
+                null;
         Integer ordinal = finalQuery.getExperienceDTO() != null ?
-                finalQuery.getExperienceDTO().getExperience().ordinal() : null;
+                finalQuery.getExperienceDTO().getExperience().ordinal() :
+                null;
         BigDecimal salary = finalQuery.getSalary() != null ?
-                finalQuery.getSalary().getSalary() : null;
+                finalQuery.getSalary().getSalary() :
+                null;
         List<String> seniorityList = finalQuery.getSeniorityDTOs() != null ?
                 finalQuery.getSeniorityDTOs().getSeniorityDTOs().stream()
                         .map(SeniorityDTO::getSeniority)
                         .map(Enum::name)
-                        .toList() : null;
+                        .toList() :
+                null;
+
         List<JobAdvertisement> jobAdvertisements = criteriaApiFindQuery(
                 localization,
                 formOfWork,
@@ -166,24 +83,18 @@ public class JobAdvertisementService {
                 seniorityList
         );
 
-        Predicate<JobAdvertisement> skillFilterPredicate = jobAdvertisement -> true;
-        if (finalQuery.getSkillDTOs() != null) {
-            List<String> skills = finalQuery.getSkillDTOs().getSkills().stream()
-                    .map(SkillDTO::getSkill)
-                    .map(Skill::name)
-                    .sorted()
-                    .toList();
-            if (finalQuery.getIsSpecifiedSkills()) {
-                skillFilterPredicate = jobAdv -> Arrays.stream(jobAdv.getSkillsNeeded().split(";"))
-                        .allMatch(skills::contains);
-            } else {
-                skillFilterPredicate = jobAdv -> Arrays.stream(jobAdv.getSkillsNeeded().split(";"))
-                        .map(skills::contains)
-                        .toList()
-                        .contains(true);
-            }
-        }
+        Predicate<JobAdvertisement> skillFilterPredicate = jobAdvertisementSkillPredicate(finalQuery);
+        Predicate<JobAdvertisement> languageFilterPredicate = jobAdvertisementLanguagePredicate(finalQuery);
 
+        jobAdvertisements = jobAdvertisements.stream()
+                .filter(skillFilterPredicate)
+                .filter(languageFilterPredicate)
+                .toList();
+
+        return jobAdvertisements;
+    }
+
+    private static Predicate<JobAdvertisement> jobAdvertisementLanguagePredicate(JobAdvertisementFinalFindQueryDTO finalQuery) {
         Predicate<JobAdvertisement> languageFilterPredicate = jobAdvertisement -> true;
         if (finalQuery.getSkillDTOs() != null) {
             List<String> languages = finalQuery.getLanguageDTOs().getLanguageDTOs().stream()
@@ -201,13 +112,28 @@ public class JobAdvertisementService {
                         .contains(true);
             }
         }
+        return languageFilterPredicate;
+    }
 
-        jobAdvertisements = jobAdvertisements.stream()
-                .filter(skillFilterPredicate)
-                .filter(languageFilterPredicate)
-                .toList();
-
-        return jobAdvertisements;
+    private static Predicate<JobAdvertisement> jobAdvertisementSkillPredicate(JobAdvertisementFinalFindQueryDTO finalQuery) {
+        Predicate<JobAdvertisement> skillFilterPredicate = jobAdvertisement -> true;
+        if (finalQuery.getSkillDTOs() != null) {
+            List<String> skills = finalQuery.getSkillDTOs().getSkills().stream()
+                    .map(SkillDTO::getSkill)
+                    .map(Skill::name)
+                    .sorted()
+                    .toList();
+            if (finalQuery.getIsSpecifiedSkills()) {
+                skillFilterPredicate = jobAdv -> Arrays.stream(jobAdv.getSkillsNeeded().split(";"))
+                        .allMatch(skills::contains);
+            } else {
+                skillFilterPredicate = jobAdv -> Arrays.stream(jobAdv.getSkillsNeeded().split(";"))
+                        .map(skills::contains)
+                        .toList()
+                        .contains(true);
+            }
+        }
+        return skillFilterPredicate;
     }
 
     public String sortCsv(String csvString) {
